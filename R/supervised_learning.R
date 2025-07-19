8b6416d0373bc7a7380817ecb1755dda4aeac685
ASuGuit <adamstruzikguit@gmail.com>
2025-07-19 08:02:55 +0200

    supervised rec_lin

--- R/supervised_learning.R
+++ R/supervised_learning.R
@@ -3,7 +3,7 @@
 #' @importFrom purrr partial
 #' @importFrom densityratio kliep
 #'
-#' @title Train a record linkage classifier
+#' @title Train a record linkage model
 #'
 #' @export
 train_rec_lin <- function(
@@ -33,26 +33,34 @@ train_rec_lin <- function(
   methods[missing_variables] <- "binary"
 
   M <- Omega[match == 1, ]
-  U <- Omega[match ==0, ]
+  U <- Omega[match == 0, ]
 
   n <- NROW(Omega)
   n_M <- NROW(M)
   pi_est <- n_M / n
 
-  if (sum(methods == "binary") > 0) {
+  binary_params <- NULL
+  continuous_parametric_params <- NULL
+  ratio_kliep <- NULL
+
+  if (any(methods == "binary")) {
+
     binary_variables <- paste0("gamma_", names(which(methods == "binary")))
     M_binary <- M[, ..binary_variables]
     U_binary <- U[, ..binary_variables]
     theta_binary <- binary_formula(M_binary)
     eta_binary <- binary_formula(U_binary)
+
     binary_params <- data.table(
       variable = binary_variables,
       theta = theta_binary,
       eta = eta_binary
     )
+
   }
 
-  if (sum(methods == "continuous_parametric") > 0) {
+  if (any(methods == "continuous_parametric")) {
+
     continuous_parametric_variables <- paste0("gamma_", names(which(methods == "continuous_parametric")))
     M_continuous_parametric <- M[, ..continuous_parametric_variables]
     U_continuous_parametric <- U[, ..continuous_parametric_variables]
@@ -60,11 +68,12 @@ train_rec_lin <- function(
     p_0_U <- p_0_formula(U_continuous_parametric)
     gamma_plus_M <- gamma_plus_formula(M_continuous_parametric)
     gamma_plus_U <- gamma_plus_formula(U_continuous_parametric)
-    temp_function <- purrr::partial(nleqslv::nleqslv, control = controls_nleqslv)
-    alpha_M <- alpha_formula(M_continuous_parametric, temp_function)
-    alpha_U <- alpha_formula(U_continuous_parametric, temp_function)
+    modified_nleqslv <- purrr::partial(nleqslv::nleqslv, control = controls_nleqslv)
+    alpha_M <- alpha_formula(M_continuous_parametric, modified_nleqslv)
+    alpha_U <- alpha_formula(U_continuous_parametric, modified_nleqslv)
     beta_M <- alpha_M / gamma_plus_M
     beta_U <- alpha_U / gamma_plus_U
+
     continuous_parametric_params <- data.table(
       variable = continuous_parametric_variables,
       p_0_M = p_0_M,
@@ -74,12 +83,15 @@ train_rec_lin <- function(
       beta_M = beta_M,
       beta_U = beta_U
     )
+
   }
 
-  if (sum(methods == "continuous_nonparametric") > 0) {
+  if (any(methods == "continuous_nonparametric")) {
+
     continuous_nonparametric_variables <- paste0("gamma_", names(which(methods == "continuous_nonparametric")))
     M_continuous_nonparametric <- M[, ..continuous_nonparametric_variables]
     U_continuous_nonparametric <- U[, ..continuous_nonparametric_variables]
+
     ratio_kliep <- do.call(
       densityratio::kliep,
       c(list(
@@ -88,8 +100,60 @@ train_rec_lin <- function(
       ),
       controls_kliep)
     )
+
   }
 
-  return(ratio_kliep)
+  structure(
+    list(
+      binary_params = if (is.null(binary_params)) NULL else binary_params,
+      continuous_parametric_params = if (is.null(continuous_parametric_params)) NULL else continuous_parametric_params,
+      ratio_kliep = if (is.null(ratio_kliep)) NULL else ratio_kliep,
+      ml_model = NULL,
+      pi_est = pi_est,
+      variables = variables,
+      comparators = comparators,
+      methods = methods
+    ),
+    class = "rec_lin_model"
+  )
+
+}
+
+#' @title Create a custom record linkage model
+#'
+#' @export
+custom_rec_lin_model <- function(ml_model, vectors) {
+
+  stopifnot("`vectors` must be of class `comparison_vectors`" =
+            is(vectors, "comparison_vectors"))
+
+  Omega <- vectors$Omega
+
+  M <- Omega[match == 1, ]
+  U <- Omega[match == 0, ]
+
+  n <- NROW(Omega)
+  n_M <- NROW(M)
+  pi_est <- n_M / n
+
+  variables <- vectors$variables
+  K <- length(variables)
+
+  methods <- replicate(K, "custom", simplify = FALSE)
+  names(methods) <- variables
+
+  structure(
+    list(
+      binary_params = NULL,
+      continuous_parametric_params = NULL,
+      ratio_kliep = NULL,
+      ml_model = ml_model,
+      pi_est = pi_est,
+      variables = variables,
+      comparators = vectors$comparators,
+      methods = methods
+    ),
+    class = "rec_lin_model"
+  )
 
 }
