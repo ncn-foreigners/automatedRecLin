db970ab6676f7935d38dd334675e1c3add032b4b
ASuGuit <adamstruzikguit@gmail.com>
2026-05-21 08:30:02 +0200

    Fix CRAN notes

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,19 @@
 if ( requireNamespace("tinytest", quietly=TRUE) ){
+  old_omp_thread_limit <- Sys.getenv("OMP_THREAD_LIMIT", unset = NA_character_)
+  Sys.setenv("OMP_THREAD_LIMIT" = "1")
+  on.exit({
+    if (is.na(old_omp_thread_limit)) {
+      Sys.unsetenv("OMP_THREAD_LIMIT")
+    } else {
+      Sys.setenv("OMP_THREAD_LIMIT" = old_omp_thread_limit)
+    }
+  }, add = TRUE)
+
+  if ( requireNamespace("data.table", quietly=TRUE) ){
+    old_dt_threads <- data.table::getDTthreads()
+    data.table::setDTthreads(1L)
+    on.exit(data.table::setDTthreads(old_dt_threads), add = TRUE)
+  }
+
   tinytest::test_package("automatedRecLin", ncpu = NULL)
 }
